50fb4ee227f4593daf4071327bb17234e7c0efb1
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-10-19 17:35:04 -0300

    when the game is finish

--- app/src/main/java/com/rodcollab/brupapp/hangman/ui/HangmanGameViewModel.kt
+++ app/src/main/java/com/rodcollab/brupapp/hangman/ui/HangmanGameViewModel.kt
@@ -1,5 +1,6 @@
 package com.rodcollab.brupapp.hangman.ui
 
+import androidx.compose.runtime.Composable
 import com.rodcollab.brupapp.hangman.domain.Trial
 import com.rodcollab.brupapp.hangman.repository.HangmanGame
 import com.rodcollab.brupapp.hangman.repository.HangmanGameImpl
@@ -16,12 +17,17 @@ data class HangmanGameUiState(
     val hits: Int = 0,
     val errors: Int = 0,
     val answer: String = "",
-)
+) {
+    val win: Boolean = usedLetters.containsAll(chars)
+    val lose: Boolean = chances == 0
+    val finished: Boolean = win || lose
+}
 
 fun Trial.toExternal() =
     HangmanGameUiState(chars, usedLetters, chances, tries, hits, errors, answer)
 
-class HangmanGameViewModel(private val repository: HangmanGame) {
+abstract class SimpleViewModel()
+class HangmanGameViewModel(private val repository: HangmanGame) : SimpleViewModel() {
 
     private val _uiState: MutableStateFlow<HangmanGameUiState> by lazy {
         MutableStateFlow(
@@ -56,3 +62,6 @@ class HangmanGameViewModel(private val repository: HangmanGame) {
         }
     }
 }
+
+@Composable
+fun <T> viewModel(factory: T) : T = factory

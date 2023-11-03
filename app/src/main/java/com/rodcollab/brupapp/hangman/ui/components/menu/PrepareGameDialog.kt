ff6f397c7c3a6ff28e0aa6f1fdaedc2bc95ea5ec
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-11-03 19:47:56 -0300

    creating PrepareGameDialog component

---
+++ app/src/main/java/com/rodcollab/brupapp/hangman/ui/components/menu/PrepareGameDialog.kt
@@ -0,0 +1,27 @@
+package com.rodcollab.brupapp.hangman.ui.components.menu
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+@Composable
+fun PrepareGameDialog() {
+    WidgetDialog {
+        TitleDialog("Preparing the game")
+        Spacer(modifier = Modifier.size(8.dp))
+        CircularProgressIndicator(strokeWidth = 2.dp)
+    }
+}
+
+@Composable
+@Preview
+fun PrepareGameDialogPreview() {
+    WidgetDialog {
+        TitleDialog("Preparing the game")
+        Spacer(modifier = Modifier.size(8.dp))
+        CircularProgressIndicator(strokeWidth = 2.dp)
+    }
+}

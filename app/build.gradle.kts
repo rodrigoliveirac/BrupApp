0f52c573e14cf70ed8ad9dc80417e11e004a325b
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-11-14 09:04:50 -0300

    update version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
